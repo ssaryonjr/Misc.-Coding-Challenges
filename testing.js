3ed4e609a9fe4085e43b20ce8bfa3cc17646ebba
Sam Saryon <ssaryonjr@gmail.com>
2022-04-20 16:22:26 -0700

    Another coding problem

--- testing.js
+++ testing.js
@@ -1,8 +1,18 @@
-function camelize(str){
-    return str
-    .split('-') //Split the sentence by each word with a dash.
-    .map((word,i) => i === 0 ? word : word[0].toUpperCase() + word.slice(1)) //Map iterate over each word and if the first iteration is the first word in the first index keep it the same, else grab the first letter in every other word index and make it upper case. Then concatenate the rest of the word with slice.
-    .join('') // Join them back together
+function matchWord(arr){
+    sentence = ''; //Created an empty sentence varaible that will concat to a string.
+    for(let i = 0; i < arr.length; i++){ //Created a double for loop to grab the rows and cols.
+        for (let j = 0; j < arr[i].length; j++){
+            sentence += arr[j][i] //Grab the first letters in each of the array's row and concatenate it into a sentence/word
+        }
+        sentence += " " //After the first loop is iterated by grabbing the first word we use this variable so it adds a space between the next word when it gets re-iterated again. 
+    }
+    return sentence;
 }
 
-console.log(camelize('we-making-money'))
+
+console.table(matchWord([
+    ['J','L','L','M'],
+    ['u','i','i','a'],
+    ['s','v','f','n'],
+    ['t','e','e','']
+    ]))
