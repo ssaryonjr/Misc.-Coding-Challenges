295f18bc30e56fe1b819b30ccfeef6e409b32fcd
Sam Saryon <ssaryonjr@gmail.com>
2022-04-21 12:42:18 -0700

    redid a problem

--- testing.js
+++ testing.js
@@ -1,1 +1,16 @@
-console.log('test')
+function test (arr){
+    let sentence = '';
+    for (let i = 0; i < arr.length; i++){
+        for (let j = 0; j < arr[i].length; j++){
+            sentence += arr[j][i]
+        }
+        sentence+= ' '
+    }
+    return sentence.trim()
+}
+
+console.table(test(
+[['J','L','L','M']
+,['u','i','i','a']
+,['s','v','f','n']
+,['t','e','e','']]))
