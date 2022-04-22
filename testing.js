1efcc26d13041de3277ea6620dbe68bc3eed6f83
Sam Saryon <ssaryonjr@gmail.com>
2022-04-22 12:32:41 -0700

    redid a problem

--- testing.js
+++ testing.js
@@ -1,1 +1,11 @@
-console.log('test')
+function isogram(str){
+    str = str.toLowerCase()
+    for (let i = 0; i < str.length; i++){
+        if(str.indexOf(str.charAt(i), i + 1) >= 0){
+            return 'characters repeat'
+        }
+    }
+    return 'no repeated characters'
+}
+
+console.log(isogram('sam'))
