ce06614a57ed444b853c6f0ea8e932092761622a
Sam Saryon <ssaryonjr@gmail.com>
2022-04-27 16:07:00 -0700

    new one

--- testing.js
+++ testing.js
@@ -1,11 +1,22 @@
-function Circle(radius){
-  radius, 
-  this.draw = function(){
-    console.log('draw it')
+function findX(arr){
+  let coordinates = [];
+
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arr[i].length; j++){
+      if (arr[i][j] == 'x'){
+        coordinates.push([i,j])
+      }
+    }
   }
+  return coordinates.length == 1 ? coordinates[0] : []
 }
 
 
-let smallCircle = new Circle(10)
-
-smallCircle = {x: 1};
+console.log(findX([
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'x', 'o', 'o'],
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+]))
