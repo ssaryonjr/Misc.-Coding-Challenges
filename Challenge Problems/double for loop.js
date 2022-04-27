e0b7b8d33d03ca8b3b8fb000aaf59669103adb05
Sam Saryon <ssaryonjr@gmail.com>
2022-04-27 16:08:01 -0700

    small changes

---
+++ Challenge Problems/double for loop.js
@@ -0,0 +1,23 @@
+function findX(arr){
+    let coordinates = [];
+  
+    for(let i = 0; i < arr.length; i++){
+      for(let j = 0; j < arr[i].length; j++){
+        if (arr[i][j] == 'x'){
+          coordinates.push([i,j])
+        }
+      }
+    }
+    return coordinates.length == 1 ? coordinates[0] : []
+  }
+  
+  
+  console.log(findX([
+    ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+    ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+    ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+    ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+    ['o', 'o', 'o', 'o', 'o', 'x', 'o', 'o'],
+    ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+  ]))
+  
